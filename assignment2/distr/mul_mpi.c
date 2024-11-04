f6c636c4028fb7501aba191e8309ea53f86c27f2
kostasfeed <kostas.damaskinakis@gmail.com>
2024-11-04 19:15:13 +0200

    finished

--- assignment2/distr/mul_mpi.c
+++ assignment2/distr/mul_mpi.c
@@ -6,12 +6,9 @@
 #include <time.h>
 #include <unistd.h>
 
-#define DEFAULT_N 2000
+#define DEFAULT_N 1000
 #define DEFAULT_P 1
 
-MPI_Status status;
-
-int P = DEFAULT_P;
 int N = DEFAULT_N;
 
 unsigned start, finish;
@@ -21,62 +18,94 @@ void init_matrices(char *fname, double A[N * N], double B[N * N], int dim);
 void *matrix_multiplication(void *);
 
 unsigned array_size;
-unsigned elems_per_thread;
-
-double A[DEFAULT_N * DEFAULT_N], B[DEFAULT_N * DEFAULT_N],
-    C[DEFAULT_N * DEFAULT_N];
+unsigned chunk_size;
 
 struct timeval the_time;
 
 int main(int argc, char *argv[]) {
 
+  double *A, *B, *C;
   int nprocs, mynum, info, source, dest;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &mynum);
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
   array_size = N * N;
-  elems_per_thread = array_size / P;
+  chunk_size = array_size / nprocs;
 
-  // assert((elems_per_thread % P) == 0); // assume there is no remainder
+  // Allocate memory for each process's chunk
+  double *A_local = malloc(chunk_size * sizeof(double));
+  double *B_local = malloc(chunk_size * sizeof(double));
+  double *C_local = malloc(chunk_size * sizeof(double));
 
-  init_matrices("matrixfile", A, B, N);
+  if (mynum == 0) {
+    A = malloc(array_size * sizeof(double));
+    B = malloc(array_size * sizeof(double));
+    C = malloc(array_size * sizeof(double));
+    init_matrices("matrixfile", A, B, N);
 
-  {
-    unsigned long now;
-    gettimeofday(&the_time, NULL);
-    now = ((the_time.tv_sec - 879191283) * 1000) + (the_time.tv_usec / 1000);
-    start = (unsigned int)now;
+    {
+      unsigned long now;
+      gettimeofday(&the_time, NULL);
+      now = ((the_time.tv_sec - 879191283) * 1000) + (the_time.tv_usec / 1000);
+      start = (unsigned int)now;
+    }
   }
-  unsigned remainder = array_size % P;
 
-  // thread_args[i].tid = i;
-  // thread_args[i].t_first_i = i * elems_per_thread;
-  // thread_args[i].t_last_i = ((i + 1) * elems_per_thread) - 1;
-  // if (i == P - 1) {
-  //   thread_args[i].t_last_i += remainder;
+  // since MPI_Scatter and MPI_Gather exist we do not need indexes
+  // unsigned myfirst, mylast;
+  // myfirst = mynum * chunk_size;
+  // mylast = ((mynum + 1) * chunk_size) - 1;
+  // unsigned remainder = array_size % nprocs;
+  // we assume that array_size % nprocs is 0
+  // if (mynum == nprocs - 1) {
+  //   mylast += remainder;
   // }
-  {
-    unsigned long now;
-    gettimeofday(&the_time, NULL);
-    now = ((the_time.tv_sec - 879191283) * 1000) + (the_time.tv_usec / 1000);
-    finish = (unsigned int)now;
-  }
-  printf("Clock count total time of %u for the whole program\n",
-         finish - start);
 
-  // print C for small N
-  FILE *outf = fopen("matrix_multiplied", "w");
-  assert(outf);
-  printf("Writing output to file: matrix_multiplied\n");
-  for (int i = 0; i < array_size; ++i) {
-    fprintf(outf, "%lf\n", C[i]);
+  MPI_Scatter(A, chunk_size, MPI_DOUBLE, A_local, chunk_size, MPI_DOUBLE, 0,
+              MPI_COMM_WORLD);
+  MPI_Scatter(B, chunk_size, MPI_DOUBLE, B_local, chunk_size, MPI_DOUBLE, 0,
+              MPI_COMM_WORLD);
+
+  // Each process computes C_local[i] = A_local[i] * B_local[i]
+  for (int i = 0; i < chunk_size; i++) {
+    C_local[i] = A_local[i] * B_local[i];
   }
-  fclose(outf);
+
+  // Gather all partial results into the root process
+  MPI_Gather(C_local, chunk_size, MPI_DOUBLE, C, chunk_size, MPI_DOUBLE, 0,
+             MPI_COMM_WORLD);
+
+  if (mynum == 0) {
+    {
+      unsigned long now;
+      gettimeofday(&the_time, NULL);
+      now = ((the_time.tv_sec - 879191283) * 1000) + (the_time.tv_usec / 1000);
+      finish = (unsigned int)now;
+    }
+    printf("Clock count total time of %u for the whole program\n",
+           finish - start);
+
+    // print C for small N
+    FILE *outf = fopen("matrix_multiplied", "w");
+    assert(outf);
+    printf("Writing output to file: matrix_multiplied\n");
+    for (int i = 0; i < array_size; ++i) {
+      fprintf(outf, "%lf\n", C[i]);
+    }
+    fclose(outf);
+    // free(A);
+    // free(B);
+    // free(C);
+  }
+  // free(A_local);
+  // free(B_local);
+  // free(C_local);
+  MPI_Finalize();
   //
   return 0;
 }
 
-void init_matrices(char *fname, double A[N * N], double B[N * N], int dim) {
+void init_matrices(char *fname, double *A, double *B, int dim) {
   assert(fname);
   FILE *f = fopen(fname, "r");
   assert(f);
@@ -87,26 +116,4 @@ void init_matrices(char *fname, double A[N * N], double B[N * N], int dim) {
     fscanf(f, "%lf\n", &B[i]);
   }
   fclose(f);
-}
-
-void *matrix_multiplication(void *args) {
-  unsigned starttime, finishtime;
-
-  {
-    unsigned long now;
-    gettimeofday(&the_time, NULL);
-    now = ((the_time.tv_sec - 879191283) * 1000) + (the_time.tv_usec / 1000);
-    starttime = (unsigned int)now;
-  }
-  // for (unsigned i = curr_thr.t_first_i; i < curr_thr.t_last_i; ++i) {
-  //   C[i] = A[i] * B[i];
-  // }
-  {
-    unsigned long now;
-    gettimeofday(&the_time, NULL);
-    now = ((the_time.tv_sec - 879191283) * 1000) + (the_time.tv_usec / 1000);
-    finishtime = (unsigned int)now;
-  }
-  // printf("Thread %u spent total time %u\n", curr_thr.tid,
-  //        finishtime - starttime);
 }
